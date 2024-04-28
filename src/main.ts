8b035bc82c00157adaa74686a5e5fb14e8a2878f
Coop, Alin <alin.coop@ataccama.com>
2024-04-28 12:32:54 +0700

    Add settings tab

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,26 @@
-import { Plugin } from "obsidian";
+import {
+	App,
+	Editor,
+	MarkdownView,
+	Modal,
+	Notice,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+} from "obsidian";
 
 interface DataEntry {
 	key: string;
 	value: number;
 }
+
+interface MyPluginSettings {
+	mySetting: string;
+}
+
+const DEFAULT_SETTINGS: MyPluginSettings = {
+	mySetting: "default",
+};
 
 function parseInput(inputString: string): DataEntry[] {
 	const data: DataEntry[] = [];
@@ -32,7 +49,24 @@ function generateBarChart(data: DataEntry[]): string {
 }
 
 export default class MyPlugin extends Plugin {
+	settings: MyPluginSettings;
+
 	async onload() {
+		await this.loadSettings();
+
+		// This adds an editor command that can perform some operation on the current editor instance
+		this.addCommand({
+			id: "insert-tinychart-template",
+			name: "Insert TinyChart template",
+			editorCallback: (editor: Editor, view: MarkdownView) => {
+				console.log(editor.getSelection());
+				editor.replaceSelection("`INSERT TEMPLATE`");
+			},
+		});
+
+		// This adds a settings tab so the user can configure various aspects of the plugin
+		this.addSettingTab(new SettingTab(this.app, this));
+
 		// This replaces csv codeblocks with tables
 		this.registerMarkdownCodeBlockProcessor("csv", (source, el) => {
 			const rows = source.split("\n").filter((row) => row.length > 0);
@@ -71,4 +105,46 @@ export default class MyPlugin extends Plugin {
 	}
 
 	onunload() {}
+
+	async loadSettings() {
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
+}
+
+class SettingTab extends PluginSettingTab {
+	plugin: MyPlugin;
+
+	constructor(app: App, plugin: MyPlugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		const { containerEl } = this;
+
+		containerEl.empty();
+
+		containerEl.createEl("h2", { text: "TinyChart Settings" });
+
+		new Setting(containerEl)
+			.setName("Setting #1")
+			.setDesc("It's a secret")
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter your secret")
+					.setValue(this.plugin.settings.mySetting)
+					.onChange(async (value) => {
+						this.plugin.settings.mySetting = value;
+						await this.plugin.saveSettings();
+					})
+			);
+	}
 }
